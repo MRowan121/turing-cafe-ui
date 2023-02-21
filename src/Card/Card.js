28c7b27f8e494ab8c19ad4bad460614db3a579e6
Matt Rowan <matt.rowan121@gmail.com>
2023-02-21 11:57:07 -0500

    Add additional details to card per comp

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -6,8 +6,8 @@ const Card = ({ name, date, time, number, id}) => {
         <div className="card">
             <h3>{name}</h3>
             <p>{date}</p>
-            <p>{time}</p>
-            <p>{number}</p>
+            <p>{time} pm</p>
+            <p>Number of guests: {number}</p>
             <button>Cancel</button>
         </div>
     )
