9c1519b2642892e9c134bf93c5594eba2971d6dd
Matt Rowan <matt.rowan121@gmail.com>
2023-02-21 12:36:03 -0500

    Add form state and jsx

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,17 +1,45 @@
 import React, { Component } from "react";
+import './Form.css'
 
 class Form extends Component {
     constructor() {
         super()
         this.state = {
-
+            name: '',
+            date: '',
+            time: '',
+            guests: '',
         }
     }
 
     render() {
         return (
-            <form>
-                
+            <form className="form">
+                <input 
+                    type='text' 
+                    placeholder="Name" 
+                    value={this.state.name}
+                    required
+                    />
+                <input 
+                    type='text' 
+                    placeholder="Date (mm/dd)" 
+                    value={this.state.date}
+                    required
+                    />
+                <input 
+                    type='text' 
+                    placeholder="Time" 
+                    value={this.state.time}
+                    required
+                    />
+                <input 
+                    type='number' 
+                    placeholder="Number of guests" 
+                    value={this.state.guests}
+                    required
+                    />
+                <button>Make Reservation</button>
             </form>
         )
     }
