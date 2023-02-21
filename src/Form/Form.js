749b7d2fdf9949f454da00ae7abd99bf8e1b8d7d
Matt Rowan <matt.rowan121@gmail.com>
2023-02-21 12:50:23 -0500

    Add on change function to form inputs

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -12,31 +12,43 @@ class Form extends Component {
         }
     }
 
+    handleChange = event => {
+        this.setState({ [event.target.name]: event.target.value })
+    }
+
     render() {
         return (
             <form className="form">
                 <input 
                     type='text' 
-                    placeholder="Name" 
+                    placeholder="Name"
+                    name="name" 
                     value={this.state.name}
+                    onChange={event => this.handleChange(event)}
                     required
                     />
                 <input 
                     type='text' 
                     placeholder="Date (mm/dd)" 
+                    name="date"
                     value={this.state.date}
+                    onChange={e => this.handleChange(e)}
                     required
                     />
                 <input 
                     type='text' 
                     placeholder="Time" 
+                    name="time"
                     value={this.state.time}
+                    onChange={e => this.handleChange(e)}
                     required
                     />
                 <input 
                     type='number' 
                     placeholder="Number of guests" 
+                    name="guests"
                     value={this.state.guests}
+                    onChange={e => this.handleChange(e)}
                     required
                     />
                 <button>Make Reservation</button>
