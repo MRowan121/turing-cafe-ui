c7daf6e1fece290960c171833bf44ae32dea3ab4
Matt Rowan <matt.rowan121@gmail.com>
2023-02-21 13:00:12 -0500

    Export add new data apicall

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -10,4 +10,21 @@ const getData = () => {
     })
 }
 
-export { getData }
+const addNewData = (name, date, time, number) => {
+    return fetch (`${url}`,
+    {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ id: Date.now(), name: name, date: date, time: time, number: number })
+    })
+    .then(res => {
+        if(!res.ok) {
+            throw new Error('Something went wrong.')
+        }
+        return res.json()
+    })
+}
+
+export { getData, addNewData }
